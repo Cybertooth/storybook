83d36f187c6ab4da24645ce6682874b960750e07
Riju Chatterjee <cybertooth.in@gmail.com>
2026-02-22 10:11:16 +0530

    feat(android): active story and story list providers

---
+++ android/lib/core/providers/active_story_provider.dart
@@ -0,0 +1,36 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import '../../domain/models/story.dart';
+import '../../domain/repositories/story_repository.dart';
+import 'repository_providers.dart';
+
+part 'active_story_provider.g.dart';
+
+@riverpod
+class StoryList extends _$StoryList {
+  @override
+  Future<List<Story>> build() async {
+    return ref.watch(storyRepositoryProvider).getAll();
+  }
+
+  Future<void> createStory(String title) async {
+    await ref.read(storyRepositoryProvider).create(title);
+    ref.invalidateSelf();
+  }
+
+  Future<void> deleteStory(String id) async {
+    await ref.read(storyRepositoryProvider).delete(id);
+    ref.invalidateSelf();
+    if (ref.read(activeStoryProvider)?.id == id) {
+      ref.read(activeStoryProvider.notifier).clear();
+    }
+  }
+}
+
+@riverpod
+class ActiveStory extends _$ActiveStory {
+  @override
+  Story? build() => null;
+
+  void set(Story story) => state = story;
+  void clear() => state = null;
+}
---
+++ android/lib/core/providers/active_story_provider.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'active_story_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$storyListHash() => r'7851d0797fdc9ba7407f06dfae36af8857c96956';
+
+/// See also [StoryList].
+@ProviderFor(StoryList)
+final storyListProvider =
+    AutoDisposeAsyncNotifierProvider<StoryList, List<Story>>.internal(
+  StoryList.new,
+  name: r'storyListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$storyListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$StoryList = AutoDisposeAsyncNotifier<List<Story>>;
+String _$activeStoryHash() => r'4f5543f5bc68ec875d64eb44e748d5277e9b9f73';
+
+/// See also [ActiveStory].
+@ProviderFor(ActiveStory)
+final activeStoryProvider =
+    AutoDisposeNotifierProvider<ActiveStory, Story?>.internal(
+  ActiveStory.new,
+  name: r'activeStoryProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$activeStoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ActiveStory = AutoDisposeNotifier<Story?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
